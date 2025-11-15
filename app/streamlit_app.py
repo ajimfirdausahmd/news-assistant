99b2b73261db28ae4b27f0991454d343df874d9f
ajimfirdausahmd <ajimfirdaus322@gmail.com>
2025-11-15 23:16:59 +0800

    Modify file

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,4 +1,12 @@
+import os
+import sys
 import streamlit as st
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))   
+ROOT_DIR = os.path.dirname(CURRENT_DIR)                    
+
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
 
 from src.graph.graph_builder import build_graph
 
